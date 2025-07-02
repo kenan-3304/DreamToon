0b19449eba9ccfa160e4af7df03ac87fd6a9bb37
kenan seremet <seremeks@dukes.jmu.edu>
2025-07-02 10:37:42 -0400

    security improvements

--- frontend/utils/supabase.ts
+++ frontend/utils/supabase.ts
@@ -2,9 +2,8 @@ import "react-native-url-polyfill/auto"; // Required for Supabase to work in Rea
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://lzrhocmfiulykdxjzaku.supabase.co"; // Paste your Project URL here
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6cmhvY21maXVseWtkeGp6YWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3MjA0NjIsImV4cCI6MjA2NDI5NjQ2Mn0.nsHFSWVuS-j73YwQztvA3SY6cUuA7CqWLoECXwFTwIM"; // Paste your Anon Key here
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
